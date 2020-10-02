98957bab14604f171c8b6b7f69399acff37a882f
danielrhoten <40212082+danielrhoten@users.noreply.github.com>
2020-10-01 17:23:10 -0700

    Day 3 updates

---
+++ Day 3/js/all-about-math.js
@@ -0,0 +1,85 @@
+// document
+// Math
+// Date
+
+
+answer = new Date();
+answer.setHours(answer.getMonth() + 20);
+
+let time = '${answer.getHours() > 12 ? answer.getHours() }: ${answer.getMinutes()} 
+${answer.getHours() > 12 ? 'PM': 'AM' }
+
+answer.setHours(answer.getMonth() + 20);
+
+const dayOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const Month = ['Jan', 'Feb', 'Mar', 'Apr'];
+const 
+
+
+console.dir (answer);
+const div = document.querySelector(selectors, 'div');
+div.innerHTML = String(answer.getMonth());
+
+
+
+Math.sqrt( x, 100);
+
+const div = document.querySelector(selectors, 'div');
+
+div.innerHTML = String(answer);
+
+const answer = Math.pow( x: 20, y: 4);
+
+const answer = Math.sign( x: 20, y: 4);
+
+
+const answer = Math.floor(x: Math.random() * 50;
+
+
+
+const isLate = confirm('is it late');
+
+const isLate = new Date().getHours() > 20;
+
+document.querySelector(selectors: 'body');
+body.style.height = '100%';
+body.style.color = 'red';
+if (isLate) {body.style.color = 'white';
+body.style.backgroundColor = 'black';}
+
+if (!isLate) {
+    body.style.color = 'red';
+    body.style.backgroundColor = 'yellow';
+}
+
+else {
+    body.style.color = 'red';
+    body.style.backgroundColor = 'yellow';
+}
+
+console.log('body', body);
+
+console.log(answer);
+
+else if (isLate && answer > 35 ) {
+    body.style.color = 'red';
+    body.style.backgroundColor = 'yellow';
+}
+
+switch (answer) {
+    case 1:
+        body.style.color = 'white';
+        body.style.backgroundColor = 'black';
+        break;
+    case 2:
+        body.style.color = 'blue';
+        body.style.backgroundColor = 'white';
+        break;
+    case 3:
+        body.style.color = 'red';
+        body.style.backgroundColor = 'yellow';
+        break;
+    default:
+        body.style.color = 'black';
+        body.style.backgroundColor = 'white';
+}
