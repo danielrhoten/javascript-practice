dec79fef1a4719fea3b60467669b2a7ec363f736
danielrhoten <40212082+danielrhoten@users.noreply.github.com>
2020-10-06 18:57:06 -0700

    Day 5 updates

---
+++ Day 5/js/scripts.js
@@ -0,0 +1,17 @@
+let a = 4;
+let b = 5;
+
+
+function myFunction(number1, number2, myString) {
+    console.log(number1 + number2);
+    return number1 + number2 + myString;
+} 
+
+const myAnonFunction = function(numnber, number2) {
+    console.log(number, numnber2)
+
+const MyFunction = (numnber, number2) => {
+    console.log(number2, numnber)
+}
+
+console.log(myFunction())
