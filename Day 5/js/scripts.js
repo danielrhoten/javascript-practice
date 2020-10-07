849438db99293d91f1c30bc620436e97ca93223f
danielrhoten <40212082+danielrhoten@users.noreply.github.com>
2020-10-06 22:11:15 -0700

    day 5 updates

--- Day 5/js/scripts.js
+++ Day 5/js/scripts.js
@@ -1,17 +1,51 @@
 let a = 4;
 let b = 5;
 
-
-function myFunction(number1, number2, myString) {
-    console.log(number1 + number2);
-    return number1 + number2 + myString;
-} 
-
-const myAnonFunction = function(numnber, number2) {
-    console.log(number, numnber2)
-
-const MyFunction = (numnber, number2) => {
-    console.log(number2, numnber)
+const person = {
+    name: 'test',
+    age: 3,
 }
 
-console.log(myFunction())
+
+function myFunction(number1, number2, myString) {
+    const a = 4;
+    const b = 5;
+    return a + b;
+} 
+
+const myFunction2(number1, number2, myString) {
+    return a + b;
+
+const MyAnonFunction = (numnber, number2) {
+    console.log(number, numnber2)
+}
+
+const MyFunction = (numnber, number2) => {
+    console.log(number, numnber2)
+}
+
+function makeMultiple(multiplier) {
+    const test = 4;
+    return function(number) {
+        return number * multiplier + test + a;
+}
+
+const makeDouble = makeMultiple (multiplier: 2);
+const makeTriple = makeMultiple (multiplier: 3);
+console.log(makeDouble(number: 10), makeTriple(number: 10)); 
+
+
+
+function makeCounter() {
+    let value = a;
+    return function () {
+        value+;
+        console.log(value);
+    }
+}
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+counter1();
+counter1()
