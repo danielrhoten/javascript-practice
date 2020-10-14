99caf84ffc516bb7c9b79d1ed6ec3f93991123c4
danielrhoten <40212082+danielrhoten@users.noreply.github.com>
2020-10-13 19:05:38 -0700

    day 7 adds

---
+++ Day 5/jseven.js
@@ -0,0 +1,8 @@
+const body = document.querySelector(selectors: 'body');
+const masterInput = createElement( type: 'input', html:, body);
+
+numberInput.type = 'number' ;
+
+numberInput.addEventListener('input', function)
+
+console.log
